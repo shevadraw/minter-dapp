c553c6bc6b44d5edebb7f19a3c08f6861551315d
shevadraw <shevad.mailbox@gmail.com>
2022-05-03 22:21:56 +0300

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x6f5C6461995B968028e3F32EcF006BAF0d50B88E"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
